907ccbbce68ad1b3d9afec8380d3019ed71c45cc
P33J3 <peterjonlawrence@gmail.com>
2021-12-12 23:57:27 -0500

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,35 +1,71 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import Home from './HomeComponent';
 //import { CAMPSITES } from '../shared/campsites';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import CampsiteInfo from './CampsiteInfoComponent';
 import  Constants  from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+
+
 
 //one stack navigator
-const DirectoryNavigator = createStackNavigator( {
-    //the components available for the stack
-    Directory: { screen: Directory },
-    CampsiteInfo: { screen: CampsiteInfo }
-},
-{
-    initialRoutineName: 'Directory',
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: '#5637DD'
-        },
-        headerTintColor:'#fff',
-        headerTitleStyle: {
-            color: '#fff'
+const DirectoryNavigator = createStackNavigator( 
+    {
+        //the components available for the stack
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRoutineName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
     }
-})
+);
+
+const HomeNavigator = createStackNavigator(
+    {
+        //the components available for the stack
+        Home: { screen: Home }
+
+    },
+    {
+        // initialRoutineName: 'Directory', (disabled because there is only one screen)
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
 
 //will return a component that handles connecting the top navigator to the react native
-const AppNavigator = createAppContainer(DirectoryNavigator);
+//const AppNavigator = createAppContainer(DirectoryNavigator);
 
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+//MainNavigator has replaced DirectoryNavigator as the top level navigator.
+const AppNavigator = createAppContainer(MainNavigator);
 class Main extends Component {
 
     //Moved the campsite data to the directory component
