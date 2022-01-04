8c4537ce7c81a3d5f665a9020e422584f2cd01aa
P33J3 <peterjonlawrence@gmail.com>
2022-01-03 19:34:22 -0500

    swipe, delete and alert functionality

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { SwipeRow } from 'react-native-swipe-list-view';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -12,6 +15,11 @@ const mapStateToProps = state => {
     };
 };
 
+//any time you want to dispatch an action Creator from a component you use mapDispatchToProps
+
+const mapDispatchToProps = {
+    deleteFavorite: campsiteId => deleteFavorite(campsiteId)
+};
 class Favorites extends Component {
 
     static navigationOptions = {
@@ -24,12 +32,46 @@ class Favorites extends Component {
         const renderFavoriteItem = ({item}) => {
 
             return (
-                <ListItem
+               <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
+               {/* expects two View components; the default view and the hidden view */}
+                    <View style={styles.deleteView}>
+                        <TouchableOpacity
+                                style={styles.deleteTouchable}
+                                // onPress={() => this.props.deleteFavorite(item.id)}
+                                onPress={() => 
+                                    Alert.alert(
+                                        'Delete Favorite?',
+                                        'Are you sure you wish to delete the favorite campsite ' +
+                                        item.name +
+                                        '?',
+                                        [
+                                            {
+                                                text: 'Cancel',
+                                                onPress: () => console.log(item.name + 'Not Deleted'),
+                                                style: 'cancel'
+                                            },
+                                            {
+                                                text: 'OK',
+                                                onPress: () => this.props.deleteFavorite(item.id)
+                                            },
+                                        ],
+                                        //by default alert can be dismissed, this disables that
+                                        { cancelable: false}
+                                    )
+                                }      
+                        >
+                                <Text style={styles.deleteText}>Delete</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                    <ListItem
                     title={item.name}
                     subtitle={item.description}
                     leftAvatar={{source: {uri: baseUrl + item.image}}}
                     onPress = {() => navigate('CampsiteInfo', {campsiteId: item.id})}
                 />
+                    </View>
+               </SwipeRow>
             )
         }
 
@@ -56,4 +98,25 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
