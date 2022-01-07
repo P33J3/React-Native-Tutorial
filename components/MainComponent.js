952eb7d7c7aed61c72856ef2cf88daf220cd8408
P33J3 <peterjonlawrence@gmail.com>
2022-01-07 13:00:25 -0500

    added local notifications

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -9,6 +9,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
 import  Constants  from 'expo-constants';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
@@ -205,6 +206,32 @@ const FavoritesNavigator = createStackNavigator(
     }
 );
 
+const LoginNavigator = createStackNavigator(
+    {
+        //the components available for the stack
+        Login: { screen: Login }
+
+    },
+    {
+      
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                            name='sign-in'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                         />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         {/* SafeAreaView is neccessary for iphone X because of its design. It is automatically included in drawer but we overrided
@@ -237,6 +264,20 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
     {
+
+        Login: { 
+            screen: LoginNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='sign-in'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         Home: { 
             screen: HomeNavigator,
             navigationOptions: {
@@ -321,6 +362,7 @@ const MainNavigator = createDrawerNavigator(
         }
     },
     {
+        initialRouteName: 'Home',
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
     }
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -6,6 +6,7 @@ import { Text, View, ScrollView, StyleSheet,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -47,6 +48,31 @@ class Reservation extends Component {
         });
     }
    
+    async presentLocalNotiification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
 
 
     render() {
@@ -119,12 +145,16 @@ class Reservation extends Component {
                                         [
                                             {
                                                 text: 'Cancel',
-                                                onPress: () => console.log( 'Not Deleted'),
+                                                onPress: () => {console.log( 'Not Deleted')
+                                                    this.resetForm()
+                                                                },
                                                 style: 'cancel'
                                             },
                                             {
                                                 text: 'OK',
-                                                onPress: () => console.log( 'Not Deleted')
+                                                onPress: () => {
+                                                    this.presentLocalNotiification(this.state.date.toLocaleDateString('en-US'));
+                                                    this.resetForm();},
                                             },
                                         ],
                                         //by default alert can be dismissed, this disables that
